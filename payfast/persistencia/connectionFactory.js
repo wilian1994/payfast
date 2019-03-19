ffbec0fd7e855e1fbe1375a47451323ba64cff8d
Wilian da Silva Oliveira <wsilva@cleartech.com.br>
2019-03-19 16:46:47 -0300

    finalizando a aula 3

---
+++ payfast/persistencia/connectionFactory.js
@@ -0,0 +1,15 @@
+var mysql = require('mysql');
+
+
+function createDBConnection(){
+    return mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'payfast'
+    });
+}
+
+module.exports = () => {
+    return createDBConnection;
+} 
